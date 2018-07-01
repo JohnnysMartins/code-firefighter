68ef778a0bdb0e65215b66570ca652028050f39f
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2018-07-01 20:07:42 -0300

    fix(mongodb); local schema url

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   server_port: process.env.PORT || 3000,
-  mongodb_url: process.env.MONGO_URL || 'localhost:27017',
+  mongodb_url: process.env.MONGO_URL || 'mongodb://localhost:27017',
   mongodb_database_name: process.env.MONGO_DB || 'firefighter'
 }
