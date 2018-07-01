2b190f18bff8b0c9843d9e70d4b11f19e0a5613b
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2018-07-01 00:37:08 -0300

    feat(middleware): updated middleware

--- src/shared/server/index.ts
+++ src/shared/server/index.ts
@@ -6,12 +6,15 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 
 import logger from './middlewares/logger';
+import exception from './middlewares/exception';
 
 const app = express();
 
-app.use(logger);
 
 app.use(bodyParser.json());
 app.use(cors())
+
+app.use(logger);
+app.use(exception);
 
 export default app;
--- src/shared/server/middlewares/logger.ts
+++ src/shared/server/middlewares/logger.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
 export default function (req: Request, res: Response, next: NextFunction) {
-  console.warn(`[${req.method}]: URL - ${req.url} IP - ${req.ip}`);
+  console.warn(`[LOG][${req.method}]: URL - ${req.url} PAYLOAD: ${JSON.stringify(req.body)}`);
   next();
 }
