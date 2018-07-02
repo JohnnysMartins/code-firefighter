ce717d7b315e640b21137bae0e75733b69af20f0
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2018-07-01 21:37:58 -0300

    refactor: remove postChecking from business

--- src/Error/ErrorBusiness.ts
+++ src/Error/ErrorBusiness.ts
@@ -12,58 +12,44 @@ const connectionClass = new ConnectionClass();
 
 export const databaseFindAll = async (req: Request, res: Response) => {
   try {
-    const result = await connectionClass.connect();
-    result.run(async () => {
-      const result = await errorController.findAll();
-      if (result.length === 0) {
-        res.status(httpStatus.NO_CONTENT).send();
-      }
-      else {
-        res.status(httpStatus.OK).send(result);
-      }
-      connection.close();
-    });
+    await connectionClass.connect();
+    const result = await errorController.findAll();
+    if (result.length === 0) {
+      res.status(httpStatus.NO_CONTENT).send();
+    }
+    else {
+      res.status(httpStatus.OK).send(result);
+    }
   }
   catch (err) {
     console.error(err);
     res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.toString());
+  }
+  finally {
     connection.close();
   }
 }
 
-export const databaseInsert = async (req: Request, res: Response, validationResult: Function) => {
+export const databaseInsert = async (req: Request, res: Response) => {
   try {
-    const result = await connectionClass.connect();
-    result.run(async () => {
-      try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(httpStatus.UNPROCESSABLE_ENTITY).json({ errors: errors.array() });
-        }
-        const body: IError = req.body;
-        const errorObject: IError = {
-          name: body.name,
-          message: body.message,
-          trace: body.trace,
-          userAgent: body.userAgent,
-          code: body.code,
-          isNodeError: body.isNodeError
-        }
-        const result = await errorController.save(errorObject);
-        res.status(httpStatus.CREATED).send(result);
-      }
-      catch (err) {
-        console.error(err);
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.toString());
-      }
-      finally {
-        connection.close();
-      }
-    });
+    await connectionClass.connect();
+    const body: IError = req.body;
+    const errorObject: IError = {
+      name: body.name,
+      message: body.message,
+      trace: body.trace,
+      userAgent: body.userAgent,
+      code: body.code,
+      isNodeError: body.isNodeError
+    }
+    const result = await errorController.save(errorObject);
+    res.status(httpStatus.CREATED).send(result);
   }
   catch (err) {
     console.error(err);
     res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.toString());
+  }
+  finally {
     connection.close();
   }
 }
