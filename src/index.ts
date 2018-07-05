172e7e3f87ad9c70168d7d5e6bc53ba55a3f7764
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2018-07-05 00:00:53 -0300

    chore: add some colors on cli

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import server from './shared/server';
 import router from './routes';
 import env from './config/env';
 import middleware from './shared/server/middlewares';
+import * as figlet from 'figlet';
 
 middleware.initMiddlewares();
 router.initRoutes();
 middleware.initExceptionMiddlewares();
 
 server.listen(env.server_port, () => {
-  console.log(`Server started on ${env.server_port}`);
+  console.log("\x1b[31m", figlet.textSync('code firefighter', {
+    font: 'Fire Font-s',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  }));
+  console.log("\x1b[0m", `Fighting fire with fire on port ${env.server_port}`);
 });
