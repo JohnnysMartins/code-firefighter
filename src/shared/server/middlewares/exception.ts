3f6abd2669eb0e6138ca7a2e00859b36617464ab
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2018-07-09 22:03:49 -0300

    fix(mongo): close connection on exception

--- src/shared/server/middlewares/exception.ts
+++ src/shared/server/middlewares/exception.ts
@@ -2,9 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 
 import * as httpStatus from 'http-status-codes';
 import GenericException from '../../exceptions/GenericException';
+import { connection } from 'mongoose';
 
 export default function (err: GenericException, req: Request, res: Response, next: NextFunction) {
   console.error(`[ERROR]: ${err.name}`);
   console.error(err.stack);
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.formatError());
+  connection.close();
 }
