0bb5743e317673a63a4c9b0a55b2d702507b1258
WandersonAlves <wandersonalvesferreira2012@gmail.com>
2018-07-10 16:33:45 -0300

    fix: get statusCode from GenericException

--- src/shared/server/middlewares/exception.ts
+++ src/shared/server/middlewares/exception.ts
@@ -1,12 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-import * as httpStatus from 'http-status-codes';
 import GenericException from '../../exceptions/GenericException';
 import { connection } from 'mongoose';
 
 export default function (err: GenericException, req: Request, res: Response, next: NextFunction) {
   console.error(`[ERROR]: ${err.name}`);
   console.error(err.stack);
-  res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.formatError());
   connection.close();
+  res.status(err.statusCode).send(err.formatError());
 }
