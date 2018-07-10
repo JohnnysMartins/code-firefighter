6d35f6694c3d1b33b5dca4f7cce63a8c1feb2ec2
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2018-07-09 22:03:34 -0300

    fix(redis): add missing env.url to connection

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,5 @@ export default {
   server_port: process.env.PORT || 3000,
   mongodb_url: process.env.MONGO_URL || 'mongodb://localhost:27017',
   mongodb_database_name: process.env.MONGO_DB || 'firefighter',
-  redis_url: process.env.REDIS_URL || 'localhost:6379'
+  redis_url: process.env.REDIS_URL || 'redis://localhost:6379'
 }
--- src/shared/class/RedisController.ts
+++ src/shared/class/RedisController.ts
@@ -1,5 +1,6 @@
 import { createClient, RedisClient } from 'redis';
 import { promisify } from 'util';
+import env from '../../config/env';
 
 export default class RedisController {
   private client: RedisClient;
@@ -7,7 +8,7 @@ export default class RedisController {
   private setAsync;
 
   constructor() {
-    this.client = createClient();
+    this.client = createClient({url: env.redis_url});
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.setAsync = promisify(this.client.set).bind(this.client);
   }
